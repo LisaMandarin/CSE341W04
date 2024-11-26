25979598bf183fdd18447f1c8fdfaff4337be488
LisaMandarin <chu23001@byui.edu>
2024-11-26 23:15:49 +0800

    auth0 done

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 require("dotenv").config()
 const express = require("express")
+const { auth } = require("express-openid-connect")
 
 const app = express()
 
-app.get("/", (req, res) => res.send("Hi"))
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+}
+
+app.use(auth(config))
+app.get("/", (req, res) => {
+    console.log("isAuthenticated: ", req.oidc.isAuthenticated())
+    console.log("user: ", req.oidc.user)
+    res.send(req.oidc.isAuthenticated()? "Logged in" : "Logged out")
+})
 
 const port = process.env.PORT
 app.listen(port, () => console.log(`Server is running on the port http://localhost:${port}`))
