2eee2b37a289e9dfccdfbe119fb2760b8b86899f
LisaMandarin <chu23001@byui.edu>
2024-11-27 01:22:29 +0800

    add swagger

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config()
 require("./mongo_db")
 const express = require("express")
+const swaggerUi = require("swagger-ui-express")
+const swaggerDocument = require("./swagger.json")
 const { auth, requiresAuth } = require("express-openid-connect")
 
 const app = express()
@@ -10,17 +12,19 @@ const config = {
     auth0Logout: true,
     secret: process.env.SECRET,
     baseURL: process.env.BASE_URL,
-    issuerBaseURL: process.env.ISSUER_BASE_URL
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
 }
 
 app.use(auth(config))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
 app.get("/", (req, res) => {
     console.log("isAuthenticated: ", req.oidc.isAuthenticated())
     res.send(req.oidc.isAuthenticated()? "Logged in" : "Logged out")
 })
 
 app.get("/profile", requiresAuth(), (req, res) => {
-    res.send(JSON.stringify(req.oidc.user))
+    res.send(JSON.stringify(req.oidc.user, null, 2))
 })
 
 const port = process.env.PORT
